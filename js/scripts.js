d53e4f20ad4691b77f355b7f1989d22a8895009f
MarcusKyung <Kyungmj@gmail.com>
2023-03-22 11:23:43 -0700

    add functionality for uppercase lettering using JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@ window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
     const messageInput = document.getElementById("messageInput").value;
-    document.querySelector("span#messageOutput").innerText = messageInput;
-    document.querySelector("span#messageOutput").style.textTransform = "uppercase";
-    document.querySelector("span#messageOutput").style.color = "red";
+    document.querySelector("span#messageOutput").innerText = messageInput.toUpperCase();
+    
+    //InlineCSS Styling Method
+    // document.querySelector("span#messageOutput").innerText = messageInput;
+    // document.querySelector("span#messageOutput").style.textTransform = "uppercase";
+    // document.querySelector("span#messageOutput").style.color = "red";
     event.preventDefault();
   };
 };
